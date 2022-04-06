a75e14e257214ba1bea921825e15788538594e6d
Manjeet Kumar Singh <manjeets1245@gmail.com>
2022-04-07 00:52:24 +0530

    changes the complete

--- src/component/Form.js
+++ src/component/Form.js
@@ -28,7 +28,7 @@ const Form = ({setStatus,inputText,setInputText,todos,setTodos}) => {
             <div className='select'>
                 <select onChange={statusHandler} name='todos' className='filter-todo'>
                     <option value='all'>All</option>
-                    <option value='completed'>Completed</option>
+                    <option value='completed'>Complete</option>
                     <option value='uncompleted'>Uncompleted</option>
                 </select>
 
